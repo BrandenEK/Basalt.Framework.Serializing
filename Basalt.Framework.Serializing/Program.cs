8b1834f8ab01667b69e54f94a16abf549797a7ae
unknown <brandenek03912@hotmail.com>
2024-05-25 17:54:24 -0400

    init: add base project files

---
+++ Basalt.Framework.Serializing/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Basalt.Framework.Serializing;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        byte type = 5;
+        uint bitfield = 0x12;
+        double dec = 5.56;
+
+        var stream = new SerializableStream();
+        stream.Write(type);
+        stream.Write(bitfield);
+        stream.Write(dec);
+
+        File.WriteAllBytes(Environment.CurrentDirectory + "/test.txt", stream);
+        byte[] bytes = stream;
+
+        foreach (var value in bytes)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Basalt.Framework.Serializing/SerializableStream.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Basalt.Framework.Serializing;
+
+public class SerializableStream
+{
+    private readonly List<byte> _bytes = new();
+
+    public void Write(byte data)
+    {
+        _bytes.Add(data);
+    }
+
+    public void Write(sbyte data)
+    {
+        _bytes.Add((byte)data);
+    }
+
+    public void Write(ushort data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public void Write(short data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public void Write(uint data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public void Write(int data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public void Write(ulong data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public void Write(long data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public void Write(float data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public void Write(double data)
+    {
+        _bytes.AddRange(BitConverter.GetBytes(data));
+    }
+
+    public static implicit operator byte[](SerializableStream stream) => stream._bytes.ToArray();
+}
